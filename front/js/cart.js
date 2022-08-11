c578b7788b3545088b40a75ec0167c0c88d24a4e
ohsand <osand.business@gmail.com>
2022-08-11 23:50:50 +0200

    order page

--- front/js/cart.js
+++ front/js/cart.js
@@ -64,10 +64,8 @@ function removeItems(event) {
 }
 
 function changeQuantity(event) {
-  //var val = document.querySelector('input').value;
   console.log(event.target.className);
   let itemToChangeQuantity = event.target.className;
-  //var val = document.getElementById(itemToChangeQuantity.val());
   var val = document.getElementsByClassName(event.target.className)[0].value;
   JSON.parse(localStorage.getItem(itemToChangeQuantity));
   console.log(JSON.parse(localStorage.getItem(itemToChangeQuantity)));
@@ -82,4 +80,122 @@ function changeQuantity(event) {
   console.log(tempProduct);
   const local = localStorage.setItem(tempProduct.productname + tempProduct.productcolor, JSON.stringify(tempProduct));
   //location.reload();
+  setTimeout(function(){
+    window.location.reload();
+ }, 1000);
+}
+
+const button = document.getElementById('order');
+
+button.addEventListener('click', function handleClick() {
+  console.log('commande');
+  var email = document.getElementById('email');
+  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  if (!filter.test(email.value)) {
+  alert('Veuillez saisir une addresse mail valide puis réessayez');
+  email.focus;
+  return false;
+}
+});
+
+/*const postForm = document.querySelector('input#order');
+postForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  const firstName = document.querySelector('#firstName').value;
+  const lastName = document.querySelector('#lastName').value;
+  const address = document.querySelector('#address').value;
+  const city = document.querySelector('#city').value;
+  const email = document.querySelector('#email').value;
+  // condition à valider avant l'envoi
+  sendInfo ();
+function sendInfo (){
+    // création des items attendu par l'api puis envoie
+    const contact = { firstName, lastName, address, city, email };
+    const products = localStorage;
+    //send(contact, products);
+    console.log(contact);
+    console.log(products);
+  }
+
+
+  
+}) */
+
+/*
+function send(contact, products) {
+  fetch('http://localhost:3000/api/products/order', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      contact, products
+    })
+  })
+    // traitement de la reponse api
+    .then(function (res) {
+      res.json().then(data => {
+        console.log(data)
+        console.log("orderId", data.orderId)
+        //isoler le string orderId
+        // let id = data[Object.keys(data)[Object.keys(data).length - 1]]
+        // passer l'id dans l'url
+        // console.log(id)
+        localStorage.removeItem("basket");
+        window.location.href = `confirmation.html?orderId=${data.orderId}`
+      })
+    })
+}
+
+
+//Create object 
+/*
+const contact {
+  firstName = 
+  lastName = 
+  address = 
+  city = 
+  email = 
+}*/
+
+const postForm = document.querySelector("input#order");
+postForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  const firstName = document.querySelector('#firstName').value;
+  const lastName = document.querySelector('#lastName').value;
+  const address = document.querySelector('#address').value;
+  const city = document.querySelector('#city').value;
+  const email = document.querySelector('#email').value;
+  // condition à valider avant l'envoi
+  if (typeof firstName !== 'undefined' &&
+    typeof lastName !== 'undefined' &&
+    typeof address !== 'undefined' &&
+    typeof city !== 'undefined' &&
+    typeof email !== 'undefined'
+  ) {
+    // création des items attendu par l'api puis envoie
+    let contact = { firstName, lastName, address, city, email };
+    let products = localStorage;
+    send(contact, products);
+    console.log(products);
+  }
+  return
+});
+
+function send(contact, products) {
+  fetch('http://localhost:3000/api/products/order', {
+    method: "POST",
+    body: JSON.stringify({
+      contact, products
+    })
+  })
+    .then(function (res) {
+      res.json().then(data => {
+        console.log(data)
+        console.log("orderId", data.orderId)
+        //window.location.href = `confirmation.html?orderId=${data.orderId}`
+      })
+    })
 }
