c72ff140ecb73fa19c9c8e1b62e6fff2e6a95f9a
ohsand <osand.business@gmail.com>
2022-08-19 21:01:40 +0200

    erase localstorage after order

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Kanap #
+cd # Kanap #
 
 This is the front end and back end server for Project 5 of the Web Developer path.
 
--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,9 @@
 let orderId = (new URL(location.href)).searchParams;
-let id = orderId.get('orderId')
-document.querySelector('#orderId').innerHTML = `${id}`
+let id = orderId.get('orderId');
+document.querySelector('#orderId').innerHTML = `${id}`;
+eraseBasket();
+storage.clear();
+
+function eraseBasket() {
+    localStorage.clear();
+};
